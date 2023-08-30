d0ef57844382e7da5cab0af8f7910fe8becf5323
Olkhovskii <98234146+Olkhovskii@users.noreply.github.com>
2023-08-30 03:41:46 +0200

    Update task_1.java

--- task_1.java
+++ task_1.java
@@ -1,1 +1,11 @@
-
+@Test
+public void checkIsAdultWhenAgeIsMoreThan18True() {
+    Program program = new Program();
+    boolean isAdult = program.checkIsAdult(19);
+	
+     boolean expected = isAdult;
+    
+    assertEquals("Возраст", expected, isAdult); 
+    
+    }
+    
